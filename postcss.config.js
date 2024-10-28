5d186ded36db6d760112472c1954962278cf4a45
JaviOHS <jharos@unemi.edu.ec>
2024-10-27 23:21:08 -0500

    Primer Commit

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/html/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/html/*.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'quicksand': ['Quicksand', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
